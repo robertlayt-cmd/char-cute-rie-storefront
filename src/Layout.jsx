209caa973c9c4b10a54d1f5fd8ca0ff195be493d
Robert Layt <robertlayt83@gmail.com>
2026-03-05 07:46:52 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import ErrorLogger from '@/components/ErrorLogger';
+import useTheme from '@/components/theme/useTheme';
 
 export default function Layout({ children }) {
   const { pathname, search } = useLocation();
+  useTheme();
 
   useEffect(() => {
     window.scrollTo(0, 0);
